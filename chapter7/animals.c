efa5bd85ac9f00054a3311ad38f1151eb3048948
fangkun <14fangkun@tongji.edu.cn>
2016-04-08 16:18:45 +0800

    chapter7 examples finished

---
+++ chapter7/animals.c
@@ -0,0 +1,34 @@
+/* animals.c -- use switch statement */
+#include <stdio.h>
+#include <ctype.h>
+int main (void)
+{
+	char ch;
+	printf("Give me a letter of the alphabet, and I will give ");
+	printf("an animal name\nbeginning with that letter.\n");
+	printf("Please type in a letter; type # to end your act.\n");
+	while ((ch = getchar()) != '#')
+	{
+		if (ch == '\n')
+			continue;
+		if (islower (ch))	/* only identify lowercase letters */
+			switch (ch)
+			{
+				case 'a':printf("argali, a wild sheep of Asia\n"); break;
+				case 'b':printf("babirusa, a wild pig of Malay\n"); break;
+				case 'c':printf("coati, racoonlike mammal\n"); break;
+				case 'd':printf("desman, aquatic, molelike critter\n"); break;
+				case 'e':printf("echidna, the spiny anteater\n"); break;
+				case 'f':printf("fisher, brownish marten\n"); break;
+				default: printf("That's a stumper!\n");
+			}		/* switch statement ends */
+		else
+			printf("I recognize only lowercase letters.\n");
+		while (getchar() != '\n')
+			continue;	/* skip the rest of input row */
+		printf("Please type another letter or a #.\n");
+	}				/* while loop ends */
+	printf("Bye!\n");
+	return 0;
+}
+
